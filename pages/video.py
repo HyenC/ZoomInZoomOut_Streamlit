ead139ead5561fdf5a6a1557a9ce9d18ba815b3e
HyenC <38906574+HyenC@users.noreply.github.com>
2023-01-05 01:25:05 +0900

    Update video.py

--- pages/video.py
+++ pages/video.py
@@ -9,10 +9,18 @@ import plotly.express as px
 st.set_page_config(
     page_title="줌인줌아웃",
     page_icon="📸",
-    layout="wide"
+    layout="centered"
 )
 
 # sidebar
 with st.sidebar:
     st.markdown("## 📸줌인줌아웃📸")
     st.markdown("#### 실시간 웹캠 자리비움 탐지 보고서")
+
+# 녹화본 파일 띄우기
+def main():
+    video_file = open("data/final.mp4", "rb")
+    st.video(video_file)  
+
+if __name__ == "__main__":
+    main()
