4a55f6e5b60895d00ac34e75b3055e96ee95595e
HyenC <38906574+HyenC@users.noreply.github.com>
2023-01-05 01:37:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 st.set_page_config(
     page_title="줌인줌아웃",
@@ -13,10 +14,5 @@ with st.sidebar:
     st.markdown("#### 실시간 웹캠 자리비움 탐지 보고서")
 
     
-def load_image(image_file):
-    img = Image.open(image_file)
-    return img
-
-load_image("data/logo.png")
-
-st.image("data/logo.png")
+image = Image.open('data/logo.png')
+st.image(image)
