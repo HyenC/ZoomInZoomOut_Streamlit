c605d16f08ffb8231c070004e8607a316dae8cd2
HyenC <38906574+HyenC@users.noreply.github.com>
2023-01-05 01:53:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,23 @@ with st.sidebar:
     st.markdown("#### 실시간 웹캠 자리비움 탐지 보고서")
 
    
+img = Image.open('data/logo.png')
+st.image(img)
+
 def main() :
     
-    img = Image.open('data/logo.png')
-    st.image(img)
-    
-    st.tabs(["Topic", "Model", "Demonstration"])
+    tab1, tab2, tab3 = st.tabs(["Topic", "Model", "Demonstration"])
+    with tab1:
+        st.header("A cat")
+#         st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+    with tab2:
+        st.header("A dog")
+#         st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+    with tab3:
+        st.header("An owl")
+#         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     
     
 if __name__ == "__main__" :
