66fb0fcbf8866136301a0695a102f43631b910a7
HyenC <38906574+HyenC@users.noreply.github.com>
2023-01-05 03:44:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.image(img1)
 
 def main() :
     
-    tab1, tab2, tab3 = st.tabs(["Topic", "Model", "Demonstration"])
+    tab1, tab2, tab3 = st.tabs(["Topic", "Model", "Output"])
     with tab1:
         st.subheader("코로나19와 비대면 원격 수업")
         st.write("신종 코로나바이러스 감염증(코로나19)으로 비대면 원격 서비스 활용이 늘어나면서, <멋쟁이사자처럼 AIS> 7기 수업 역시 줌(Zoom)을 통한 온라인 수업으로 진행되고 있습니다.")
@@ -31,19 +31,32 @@ def main() :
         selected_item = st.radio("", ("Mediapipe", "OpenCV", "Face Recognition", "Yolo"), horizontal=True)
 
         if selected_item == "Mediapipe":
-              st.write("A!")
+		st.subheader("Mediapipe")
+              	st.write("A!")
         elif selected_item == "OpenCV":
-              st.write("B!!")
+		st.subheader("OpenCV")
+              	st.write("B!!")
         elif selected_item == "Face Recognition":
-              st.write("C!!!")
+		st.subheader("Face Recognition")
+              	st.write("C!!!")
         else:
-              st.write("D!!!!")
+		st.subheader("Yolo")
+              	col1, col2, col3 = st.columns(3)
+
+		with col1:
+			st.header("Yolov3")
+   			st.image("https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3eb0dd96-562d-4937-a766-b1789baa301e/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230104%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230104T184234Z&X-Amz-Expires=86400&X-Amz-Signature=8c93dae77a6959f98243e67ede33b2d907708e190116f3a1d8e9a191b761d688&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject")
+
+		with col2:
+   			st.header("Yolov5")
+   			st.image("https://s3.us-west-2.amazonaws.com/secure.notion-static.com/e5412de4-0d2d-4eb1-94da-d538c99dd641/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230104%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230104T184317Z&X-Amz-Expires=86400&X-Amz-Signature=f11e4e6cde071c027320b2fa59f7d079065efc5200c8c313c8cf39fa95139c46&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject")
+
+		with col3:
+   			st.header("Yolov7")
+   			st.image("https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a6c530d4-d901-4d0f-9089-74cb2eed2294/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230104%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230104T184341Z&X-Amz-Expires=86400&X-Amz-Signature=53ba0c07f9486669f4aff96ed0368da2d4ba08ee99b402474789469bb371770e&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject")
 
     with tab3:
-        video_file = open("data/final.mp4", "rb")
-        video_bytes = video_file.read()
-	
-        st.video(video_bytes)
+	st.write("무엇을 넣을까요")
     
     
 if __name__ == "__main__" :
