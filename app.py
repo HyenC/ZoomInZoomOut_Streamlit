d6211567a256ba690b2b94f3fec2d3428c5938b8
HyenC <38906574+HyenC@users.noreply.github.com>
2023-01-05 13:00:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def main() :
         selected_item = st.radio("", ("Mediapipe", "OpenCV", "Face Recognition", "Yolo"), horizontal=True, label_visibility="collapsed")
 
         if selected_item == "Mediapipe":
-            st.header("Mediapipe")
+            st.write("")
             st.image("data/mediapipe.png")
             st.write(f"""
                 - 구글에서 주로 인체를 대상으로 하는 비전인식기능들을 AI모델 개발과 기계 학습까지 마친 상태로 제공하는 서비스
@@ -42,10 +42,8 @@ def main() :
                 - 6개의 랜드마크(오른쪽 눈, 왼쪽 눈, 코 끝, 입 중심, 오른쪽 귀 윗 가장자리 위의 점 및 왼쪽 귀 윗 가장자리 위의 점) 및 다중 얼굴 지원과 함께 제공되는 초고속 얼굴 감지 솔루션
             """)
         elif selected_item == "OpenCV":
-            st.header("OpenCV")
-            st.image("data/opencv.png", width=400)
+            st.write("")
             st.image("data/s_opencv.png", width=400)
-            st.image("data/opencv_s.png", width=250)
             st.write(f"""
                 - Open Source Computer Vision의 약자로, 영상 처리에 사용할 수 있는 오픈 소스 라이브러리
                 - 컴퓨터가 사람의 눈처럼 인식할 수 있게 처리해주는 역할을 하기도 하며, 카메라 어플에서도 OpenCV가 사용됨
